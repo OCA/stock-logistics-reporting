cd948a68799b400c3860bc0a481fe3d75648d176
acysos <info@acysos.com>
2014-11-30 21:20:31 +0100

    [FIX] stock_valued: fix travis

--- stock_valued/models/__init__.py
+++ stock_valued/models/__init__.py
@@ -18,5 +18,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import res_partner
-import stock_valued
+from . import res_partner
+from . import stock_valued
