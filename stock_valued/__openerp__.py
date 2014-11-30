e707563ab44eea73e367129d359f3b2abb95ca1e
acysos <info@acysos.com>
2014-11-30 20:51:46 +0100

    [ADD] stock_valued: Add amount information to pickings

---
+++ stock_valued/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (c) 2014 Acysos S.L. (http://acysos.com) All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Stock valued",
+    "version": "1.0",
+    "author": "Acysos S.L.",
+    "category": "Stock Management",
+    "summary": "Add amount information to pickings.",
+    "license": "AGPL-3",
+    "depends": ["base",
+                "sale",
+                "sale_stock",
+                ],
+    "init_xml": [],
+    "data": ['views/res_partner_view.xml',
+             'views/stock_valued_view.xml',
+             'views/report_stock_valued.xml',
+             ],
+    "active": False,
+    "installable": True,
+}
---
+++ stock_valued/models/stock_valued.py
@@ -0,0 +1,75 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Odoo, Open Source Management Solution
+#    Copyright (c) 2014 Acysos S.L. (http://acysos.com) All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+from openerp.addons.decimal_precision import decimal_precision as dp
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.one
+    @api.depends('sale_id.amount_untaxed', 'sale_id.amount_tax',
+                 'sale_id.amount_total')
+    def _compute_amount(self):
+        if self.sale_id:
+            self.amount_untaxed = self.sale_id.amount_untaxed
+            self.amount_tax = self.sale_id.amount_tax
+            self.amount_total = self.sale_id.amount_total
+
+    amount_untaxed = fields.Float(compute='_compute_amount',
+                                  digits_compute=dp.get_precision('Account'),
+                                  string='Untaxed Amount')
+    amount_tax = fields.Float(compute='_compute_amount',
+                              digits_compute=dp.get_precision('Account'),
+                              string='Taxes')
+    amount_total = fields.Float(compute='_compute_amount',
+                                digits_compute=dp.get_precision('Account'),
+                                string='Total')
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.one
+    @api.depends('procurement_id.sale_line_id.price_unit',
+                 'procurement_id.sale_line_id.discount',
+                 'procurement_id.sale_line_id.price_subtotal')
+    def _sale_prices(self):
+        if self.procurement_id.sale_line_id:
+            sale_line = self.procurement_id.sale_line_id
+            self.sale_price_subtotal = sale_line.price_subtotal
+            self.sale_price_unit = sale_line.price_unit
+            self.sale_discount = sale_line.discount
+        else:
+            self.sale_price_subtotal = 0
+            self.sale_price_unit = 0
+            self.sale_discount = 0
+
+    sale_price_subtotal = fields.Float(
+        compute='_sale_prices', digits_compute=dp.get_precision('Account'),
+        string='Subtotal')
+    sale_price_unit = fields.Float(compute='_sale_prices',
+                                   digits_compute=dp.get_precision('Account'),
+                                   string='Price')
+    sale_discount = fields.Float(compute='_sale_prices',
+                                 digits_compute=dp.get_precision('Account'),
+                                 string='Discount (%)')
