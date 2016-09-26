2c03b6df779ace069d5e97c36a83d49fe8de1e0d
Angel A. Guadarrama B <fr33codev@openmailbox.org>
2016-09-26 15:25:51 -0400

    FIX - Restore original author

--- stock_analysis/__openerp__.py
+++ stock_analysis/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2016 - I.A.S. Ingenieria, Aplicaciones y Software
+# © 2016 Lorenzo Battistini - Agile Business Group - I.A.S. Ingenieria, Aplicaciones y Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Analysis",
     "summary": "Analysis view for stock",
     "version": "9.0.1.3",
     "category": "Inventory, Logistic, Storage",
-    "website": "http://www.ias.com.co/",
-    "author": "I.A.S. Ingenieria, Aplicaciones y Software",
+    "website": "https://www.agilebg.com - http://www.ias.com.co/",
+    "author": "Agile Business Group, Odoo Community Association (OCA) & I.A.S. Ingenieria, Aplicaciones y Software",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
--- stock_analysis/report/stock_analysis.py
+++ stock_analysis/report/stock_analysis.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 - I.A.S. Ingenieria, Aplicaciones y Software
+# © 2016 Lorenzo Battistini - Agile Business Group - I.A.S. Ingenieria, Aplicaciones y Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import tools
