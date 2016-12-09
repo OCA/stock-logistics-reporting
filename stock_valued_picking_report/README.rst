eb48954236dc74fd2aaf381d1ba49d31c581db8a
Carlos Dauden <carlos.dauden@tecnativa.com>
2016-12-09 21:00:37 +0100

    [MIG] stock_valued_picking_report: Migration to 9.0

--- stock_valued_picking_report/README.rst
+++ stock_valued_picking_report/README.rst
@@ -1,21 +1,69 @@
-Valued pickings
-===============
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
 
-Add amount information to picking list report and delivery order view.
+===========================
+Stock Valued Picking Report
+===========================
+
+Add amount information to Delivery Slip report.
 You can select at partner level if picking list report must be valued or not.
 
-Installation
-============
 
-To install this module, stock_picking_taxes is required, that it's available at:
+Usage
+=====
 
-https://github.com/odoomrp/odoomrp-wip
+To use this module, you need:
+
+#. Go to Customers
+#. Set valued field
+#. Create a Sale Order with stockable products
+#. Confirm Sale Order
+#. Click Deliveries button
+#. Go to Print > Delivery Slip
+
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/151/9.0
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/stock-logistics-reporting/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
 
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
+
 Contributors
 ------------
-* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-* Antonio Espinosa <antonioea@antiun.com>
+
+* Pedro M. Baeza <pedro.baeza@tecnativa.com>
+* Antonio Espinosa <antonio.espinosa@tecnativa.com>
 * Oihane Crucelaegui <oihane.crucelaegi@avanzosc.es>
+* Carlos Dauden <carlos.dauden@tecnativa.com>
+
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- stock_valued_picking_report/__openerp__.py
+++ stock_valued_picking_report/__openerp__.py
@@ -1,46 +1,26 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#    Copyright (c) 2015 Antiun Ingenieria (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2014 Pedro M. Baeza - Tecnativa <pedro.baeza@tecnativa.com>
+# Copyright 2015 Antonio Espinosa - Tecnativa <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Valued picking list",
-    "version": "8.0.0.2.0",
-    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza",
-    "website": "www.serviciosbaeza.com",
+    "name": "Stock Valued Picking Report",
+    "summary": "Adding Valued Picking on Delivery Slip report",
+    "version": "9.0.1.0.0",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
     "category": "Warehouse Management",
-    "license": "GPL-3",
+    "license": "AGPL-3",
     "depends": [
-        "base",
         "account",
         "stock",
         "sale",
         "delivery",
-        "stock_picking_taxes",
-        "web_context_tunnel"
     ],
     "data": [
         'views/res_partner_view.xml',
-        'views/stock_picking_view.xml',
         'report/stock_picking_valued_report.xml',
     ],
     "installable": True
