9dc73ce62e56d29eeccbaa2f4b6ebc20d3f289c5
cubells <vicent@vcubells.net>
2017-05-15 15:44:38 +0200

    [MIG] stock_picking_report_custom_description: Migration to v11

--- stock_picking_report_custom_description/models/stock.py
+++ stock_picking_report_custom_description/models/stock.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
-class StockPackOperation(models.Model):
-    _inherit = 'stock.pack.operation'
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
 
     several_product_description = fields.Boolean(
         compute='_compute_several_product_description',
@@ -15,6 +14,6 @@ class StockPackOperation(models.Model):
     @api.multi
     def _compute_several_product_description(self):
         for operation in self:
-            names = operation.linked_move_operation_ids.mapped('move_id.name')
+            names = operation.mapped('move_id.name')
             operation.several_product_description = (
                 not all(x == names[0] for x in names))
