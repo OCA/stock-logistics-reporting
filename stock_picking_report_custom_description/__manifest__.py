b938cc5011157434c7037678959a68fd21f45b54
cubells <vicent@vcubells.net>
2017-05-15 15:44:38 +0200

    [MIG] stock_picking_report_custom_description: Migration to v11

--- stock_picking_report_custom_description/__manifest__.py
+++ stock_picking_report_custom_description/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Picking Report Custom Description",
     "summary": "Show moves description in picking reports",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Warehouse",
-    "website": "http://www.tecnativa.com",
+    "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
