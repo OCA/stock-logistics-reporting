c46c75a370de12ba0f961cb23e2188a53d21a52d
David Vidal <chienandalu@gmail.com>
2017-08-17 17:05:33 +0200

    [IMP] stock_valued_picking_report: Tests and comments

--- stock_valued_picking_report/__openerp__.py
+++ stock_valued_picking_report/__openerp__.py
@@ -2,6 +2,7 @@
 # Copyright 2014 Pedro M. Baeza - Tecnativa <pedro.baeza@tecnativa.com>
 # Copyright 2015 Antonio Espinosa - Tecnativa <antonio.espinosa@tecnativa.com>
 # Copyright 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# Copyright 2017 David Vidal - Tecnativa <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -23,5 +24,5 @@
         'views/res_partner_view.xml',
         'report/stock_picking_valued_report.xml',
     ],
-    "installable": True
+    "installable": True,
 }
