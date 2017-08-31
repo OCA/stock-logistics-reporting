3c0ab130214436c01427837dabac972efaa9960b
David Vidal <chienandalu@gmail.com>
2017-08-31 09:19:20 +0200

    [MIG] stock_valued_picking_report: Migration to 10.0

--- stock_valued_picking_report/models/res_partner.py
+++ stock_valued_picking_report/models/res_partner.py
@@ -4,7 +4,7 @@
 # Copyright 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
