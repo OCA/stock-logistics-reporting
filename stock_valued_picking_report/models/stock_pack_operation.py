20745e913b86bc2d76a8424c26f08087a3fa0b53
Luis M. Ontalba <luismaront@gmail.com>
2017-10-19 20:24:03 +0200

    fixup! fixup! fixup!  [10.0][FIX] stock_valued_picking_report

--- stock_valued_picking_report/models/stock_pack_operation.py
+++ stock_valued_picking_report/models/stock_pack_operation.py
@@ -2,7 +2,6 @@
 # Copyright 2014 Pedro M. Baeza - Tecnativa <pedro.baeza@tecnativa.com>
 # Copyright 2015 Antonio Espinosa - Tecnativa <antonio.espinosa@tecnativa.com>
 # Copyright 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
-# Copyright 2017 Luis M. Ontalba - Tecnativa <luis.martinez@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
@@ -54,41 +53,42 @@ class StockPackOperation(models.Model):
 
     @api.multi
     def sale_lines_values(self, sale_lines):
-        sum_qty = sum_price = sum_discount = sum_amount_untaxed = 0.0
-        sum_amount_tax = sum_amount_total = 0.0
-        rm = sale_lines[0].order_id.company_id.tax_calculation_rounding_method
-        for sale_line in sale_lines:
-            sum_qty += sale_line.product_uom_qty
-            sum_price += sale_line.price_unit * sale_line.product_uom_qty
-            sum_discount += sale_line.discount * sale_line.product_uom_qty
-            amount_untaxed = sale_line.product_uom_qty * (
-                sale_line.price_unit * (
-                    1 - (sale_line.discount or 0.0) / 100.0))
-            sum_amount_untaxed += amount_untaxed
-            price = sale_line.price_unit * (1 - (sale_line.discount or
-                                                 0.0) / 100)
-            taxes = sale_line.tax_id.compute_all(
-                price,
-                sale_line.currency_id,
-                sale_line.product_uom_qty,
-                product=sale_line.product_id,
-                partner=sale_line.order_id.partner_id
-            )
-            if rm == 'round_globally':
-                amount_tax = sum(
-                    t.get('amount', 0.0) for t in taxes.get('taxes', []))
-            else:
-                amount_tax = taxes['total_included'] - taxes['total_excluded']
-            sum_amount_tax += amount_tax
-            sum_amount_total += amount_untaxed + amount_tax
+        if len(sale_lines) <= 1:
+            price_unit = sale_lines.price_unit
+            discount = sale_lines.discount
+        else:
+            sum_qty = 0.0
+            sum_price = 0.0
+            sum_discount = 0.0
+            for sale_line in sale_lines:
+                sum_qty += sale_line.product_uom_qty
+                sum_price += sale_line.price_unit * sale_line.product_uom_qty
+                sum_discount += sale_line.discount * sale_line.product_uom_qty
+            price_unit = sum_price / (sum_qty or 1)
+            discount = sum_discount / (sum_qty or 1)
+        price_reduce = price_unit * (1 - (discount or 0.0) / 100.0)
         sale_line = sale_lines[:1]
+        sale_tax = sale_line.tax_id
+        taxes = sale_tax.compute_all(
+            price_unit=price_reduce,
+            currency=sale_line.currency_id,
+            quantity=self.product_qty,
+            product=sale_line.product_id,
+            partner=sale_line.order_id.partner_id)
+        if sale_line.company_id.tax_calculation_rounding_method == (
+                'round_globally'):
+            price_tax = sum(
+                t.get('amount', 0.0) for t in taxes.get('taxes', []))
+        else:
+            price_tax = taxes['total_included'] - taxes['total_excluded']
         return {
             'sale_line': sale_line,
+            'sale_tax_id': sale_tax,
             'sale_tax_description': ', '.join(map(lambda x: (
-                x.description or x.name), sale_lines.mapped('tax_id'))),
-            'sale_price_unit': sum_price / (sum_qty or 1),
-            'sale_discount': sum_discount / (sum_qty or 1),
-            'sale_price_subtotal': sum_amount_untaxed,
-            'sale_price_tax': sum_amount_tax,
-            'sale_price_total': sum_amount_total,
+                x.description or x.name), sale_tax)),
+            'sale_price_unit': price_unit,
+            'sale_discount': discount,
+            'sale_price_subtotal': taxes['total_excluded'],
+            'sale_price_tax': price_tax,
+            'sale_price_total': taxes['total_included'],
         }
