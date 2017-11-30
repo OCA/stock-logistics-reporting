d1854068e9fecd9917e80c3c9a9f78e469b6be2d
Luis M. Ontalba <luis.martinez@tecnativa.com>
2017-11-30 22:13:51 +0100

    [10.0][FIX] stock_valued_picking_report: Fix report (#38)

--- stock_valued_picking_report/tests/test_stock_picking_valued.py
+++ stock_valued_picking_report/tests/test_stock_picking_valued.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - David Vidal
+# Copyright 2017 Tecnativa - Luis M. Ontalba
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests import common
@@ -27,7 +28,10 @@ class TestStockPickingValued(common.SavepointCase):
         cls.sale_order = cls.env['sale.order'].create({
             'partner_id': cls.partner.id,
             'order_line': [(0, 0, {'product_id': cls.product.id})],
+            'company_id': cls.env.user.company_id.id,
         })
+        cls.sale_order.company_id.tax_calculation_rounding_method = (
+            'round_per_line')
 
     def test_01_confirm_order(self):
         self.assertTrue(self.partner.valued_picking)
@@ -46,3 +50,13 @@ class TestStockPickingValued(common.SavepointCase):
             self.assertEqual(picking.amount_untaxed, 0.0)
             self.assertEqual(picking.amount_tax, 0.0)
             self.assertEqual(picking.amount_total, 0.0)
+
+    def test_03_tax_rounding_method(self):
+        self.sale_order.company_id.tax_calculation_rounding_method = (
+            'round_globally')
+        self.sale_order.action_confirm()
+        self.assertTrue(len(self.sale_order.picking_ids))
+        for picking in self.sale_order.picking_ids:
+            self.assertEqual(picking.amount_untaxed, 100.0)
+            self.assertEqual(picking.amount_tax, 15.0)
+            self.assertEqual(picking.amount_total, 115.0)
