2d29d52e78b4d4b51a98ebc7412801ca99f7521c
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-12-08 20:06:34 +0100

    [11.0][ADD] stock_account_valuation_report

---
+++ stock_account_valuation_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+#   (<https://www.eficent.com>)
+# Copyright 2018 Aleph Objects, Inc.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Account Valuation Report",
+    "version": "11.0.1.0.0",
+    "summary": "Improves logic of the Inventory Valuation Report",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "category": "Warehouse Management",
+    "depends": ["stock_account"],
+    "license": "AGPL-3",
+    "data": [
+        "views/product_product_views.xml",
+    ],
+    'installable': True,
+}
---
+++ stock_account_valuation_report/readme/DESCRIPTION.rst
@@ -0,0 +1,8 @@
+When you trigger a report of inventory valuation, and you use
+perpetual inventory, you should be able to reconcile the valuation
+from an inventory perspective with the valuation
+from an accounting perspective.
+
+This module changes the report in *Inventory / Reporting / Inventory Valuation*
+to display separately the Quantity and Value of each product for the
+Inventory and the Accounting systems .
