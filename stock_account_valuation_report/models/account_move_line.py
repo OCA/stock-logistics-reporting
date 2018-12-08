b990ad955211e70f07f9dc4124743b16fb295cf9
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-12-08 20:06:34 +0100

    [11.0][ADD] stock_account_valuation_report

---
+++ stock_account_valuation_report/models/account_move_line.py
@@ -0,0 +1,10 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    product_id = fields.Many2one(index=True)
