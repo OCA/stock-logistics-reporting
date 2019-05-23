ae62759c40dbb5c7c7e023b7bcb0771adba85521
emagdalena <emagdalena@c2i.es>
2019-05-23 13:26:24 +0200

    [ADD] stock_picking_comment_template

---
+++ stock_picking_comment_template/models/res_partner.py
@@ -0,0 +1,13 @@
+# Copyright 2019 C2i Change 2 improve - Eduardo Magdalena <emagdalena@c2i.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    comment_template_id = fields.Many2one(
+        comodel_name='base.comment.template',
+        string='Conditions template',
+    )
