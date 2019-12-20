b033238a2b823f8ac68bec51ee8b800bceb1f0e2
hveficent <hector.villarreal@forgeflow.com>
2019-12-20 15:49:40 +0100

    [IMP] stock_quantity_history_location: black, isort

--- stock_quantity_history_location/__manifest__.py
+++ stock_quantity_history_location/__manifest__.py
@@ -3,18 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Quantity History Location',
-    'summary': "Provides stock quantity by location on past date",
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Eficent,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-reporting',
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'wizards/stock_quantity_history.xml',
-    ],
-
+    "name": "Stock Quantity History Location",
+    "summary": "Provides stock quantity by location on past date",
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Eficent," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "depends": ["stock"],
+    "data": ["wizards/stock_quantity_history.xml"],
 }
