c70b32efd3fbe1b1f93310479310eceb1082b1b4
hveficent <hector.villarreal@forgeflow.com>
2019-12-20 16:56:14 +0100

    [MIG] stock_quantity_history_location: Migration to 13.0

--- stock_quantity_history_location/__manifest__.py
+++ stock_quantity_history_location/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 ForgeFlow S.L.
 # Copyright 2019 Aleph Objects, Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Quantity History Location",
     "summary": "Provides stock quantity by location on past date",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
-    "author": "Eficent," "Odoo Community Association (OCA)",
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "depends": ["stock"],
     "data": ["wizards/stock_quantity_history.xml"],
