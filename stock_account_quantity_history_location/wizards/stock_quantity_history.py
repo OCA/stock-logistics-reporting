e527ea7704b92c86c4bc913c0a14238dbf0b9263
david <david.vidal@tecnativa.com>
2019-12-30 13:49:13 +0100

    [FIX] stock_quantity_history_location: qty by location
    When a qty at date is asked the location context could be overriden if
    the company_owned one was inserted after we removed it. We ensure to
    remove it to always obtain the right values.
    
    As a side effect, stock_account valuation is now correctly computed when
    AVCO or Standard methods are implied. So should be with FIFO manual (as
    it computes the quantities the same way) but doubtful in FIFO real time.

--- stock_account_quantity_history_location/wizards/stock_quantity_history.py
+++ stock_account_quantity_history_location/wizards/stock_quantity_history.py
@@ -9,11 +9,28 @@ class StockQuantityHistory(models.TransientModel):
     _inherit = 'stock.quantity.history'
 
     def open_table(self):
-        action = super(StockQuantityHistory, self).open_table()
+        """Stock valuation goes by a different path than stock report, so
+           we ensure the correct context as well"""
+        action = super().open_table()
+        if not self.env.context.get('valuation'):
+            return action
         # Show 0 quantities on Inventory Valuation to display Account Valuation
         # anomalies, such as, non 0 stock_value on cost_method FIFO
-        if self.env.context.get('valuation') and not self.location_id:
+        if not self.location_id:
             domain = ast.literal_eval(action['domain'])
             domain.pop(domain.index(('qty_available', '!=', 0)))
             action['domain'] = domain
+        ctx = action['context']
+        if isinstance(ctx, str):
+            ctx = ast.literal_eval(ctx)
+        if self.location_id:
+            ctx['location'] = self.location_id.id
+            ctx['compute_child'] = self.include_child_locations
+            if ctx.get('company_owned', False):
+                ctx.pop('company_owned')
+            # Ensure the context isn't added later and catch it
+            ctx['drop_company_owned'] = True
+            action['name'] = '%s (%s)' % (action['name'],
+                                          self.location_id.complete_name)
+            action['context'] = ctx
         return action
