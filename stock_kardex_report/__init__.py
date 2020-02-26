84d7f087d517c146578f835c5abd673a25a9348a
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-02-26 09:10:04 -0600

    +[ADD] stock_kardex_report: new module to show the movements and balance of a product in a location

---
+++ stock_kardex_report/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2020, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lpgl.html).
+
+from . import models
+from . import wizard
---
+++ stock_kardex_report/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* `Jarsa Sistemas, S.A. de C.V. <http://www.jarsa.com.mx>`_:
+
+  * Juventino Villegas <juventino.villegas@jarsa.com.mx>
