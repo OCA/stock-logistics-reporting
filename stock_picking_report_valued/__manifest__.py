c29875bf620ceef391a5d028f7c937a646ab071d
Denis Roussel <denis.roussel@acsone.eu>
2020-05-13 08:50:06 +0200

    [13.0][FIX] E741 ambiguous variable name 'l'

--- stock_picking_report_valued/__manifest__.py
+++ stock_picking_report_valued/__manifest__.py
@@ -8,7 +8,7 @@
 {
     "name": "Valued Picking Report",
     "summary": "Adding Valued Picking on Delivery Slip report",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "category": "Warehouse Management",
