16b0d4fb8e48139272c5d536dcd6a2e5097f8599
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-07-04 10:03:14 +0300

    [IMP] stock_picking_comment_template: black, isort, prettier

--- stock_picking_comment_template/__manifest__.py
+++ stock_picking_comment_template/__manifest__.py
@@ -7,20 +7,15 @@
     "version": "12.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-reporting",
-    "author": "C2i Change 2 improve,"
-              "Odoo Community Association (OCA)",
+    "author": "C2i Change 2 improve," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "depends": [
-        "stock",
-        "sale",
-        "base_comment_template",
-    ],
+    "depends": ["stock", "sale", "base_comment_template"],
     "data": [
-        'views/base_comment_template_view.xml',
-        'views/report_picking.xml',
-        'views/report_delivery_document.xml',
+        "views/base_comment_template_view.xml",
+        "views/report_picking.xml",
+        "views/report_delivery_document.xml",
         "views/stock_picking_view.xml",
-        'security/ir.model.access.csv',
+        "security/ir.model.access.csv",
     ],
-    'installable': True,
+    "installable": True,
 }
