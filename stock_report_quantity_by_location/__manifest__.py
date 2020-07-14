9fc450f59e793797a50291836d70745390e6a372
kranokporn <kranokporn.td@gmail.com>
2020-07-14 17:52:49 +0700

    [IMP] stock_report_quantity_by_location: black, isort, prettier

--- stock_report_quantity_by_location/__manifest__.py
+++ stock_report_quantity_by_location/__manifest__.py
@@ -3,18 +3,12 @@
 {
     "name": "Stock Report Quantity By Location",
     "summary": "Stock Report Quantity By Location",
-    "version": "12.0.1.0.0",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "category": "Warehouse Management",
     "license": "AGPL-3",
-    "depends": [
-        "product",
-        "stock",
-    ],
-    "data": [
-        'wizards/stock_report_quantity_by_location_views.xml',
-    ],
+    "depends": ["product", "stock"],
+    "data": ["wizards/stock_report_quantity_by_location_views.xml"],
     "installable": True,
 }
