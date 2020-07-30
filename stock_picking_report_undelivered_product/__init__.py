ee6589fe91423ec56d7e9f116250f8c186e3271c
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-07-30 14:49:00 +0200

    [ADD] stock_picking_report_undelivered_product: New module to display undelivered products in delivery slip report

---
+++ stock_picking_report_undelivered_product/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2020 Sergio Teruel - Tecnativa <sergio.teruel@tecnativa.com>
+from . import models
---
+++ stock_picking_report_undelivered_product/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2020 Sergio Teruel - Tecnativa <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock picking report undelivered products",
+    "summary": "Display on picking report delivery slip undelivered products",
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa,Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
+    "category": "Warehouse",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        'views/product_views.xml',
+        'views/report_deliveryslip.xml',
+        'views/res_config_settings_views.xml',
+        'views/res_partner_view.xml',
+    ],
+    'installable': True,
+}
