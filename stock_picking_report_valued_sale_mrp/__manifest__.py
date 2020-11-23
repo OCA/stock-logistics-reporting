7a94e2e3c82fd25aa6c10cb7f93be76430fa11f1
david <david.vidal@tecnativa.com>
2020-11-23 18:30:30 +0100

    [ADD] stock_picking_report_valued_sale_mrp: New module

---
+++ stock_picking_report_valued_sale_mrp/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Valued picking linked with MRP Kits",
+    "summary": "Allow to summarize the picking related with the selled kits",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": [
+        "stock_picking_report_valued",
+        "sale_mrp",
+    ],
+    "data": [
+        "report/stock_picking_report_valued.xml",
+    ],
+}
---
+++ stock_picking_report_valued_sale_mrp/readme/USAGE.rst
@@ -0,0 +1,7 @@
+To test this module:
+
+#. Configure some products with traceability and make them kit components.
+#. Sell the kits and go to the stock picking.
+#. Print the valued delivery slip:
+   - The total amount will now be correct.
+   - The kit components will be summarized in a single kit product line.
