1e1816d2f980c3ea7bb36aa64010b2ce5eeec341
Quentin Dupont <perso@quentindupont.fr>
2020-11-30 18:13:36 +0100

    [12.0][ADD] Report picking summary

---
+++ stock_picking_report_summary/models/picking_summary_wizard_picking.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class PickingSummaryWizardPicking(models.TransientModel):
+    _name = 'picking.summary.wizard.picking'
+
+    wizard_id = fields.Many2one(comodel_name='picking.summary.wizard')
+
+    picking_id = fields.Many2one(comodel_name='stock.picking')
