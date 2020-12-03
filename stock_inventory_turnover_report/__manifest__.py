c4ddd8f99a6a3038a5a47d9f860b718d70c3c602
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-12-03 21:22:11 +0100

    [FIX] stock_inventory_turnover_report: Proper development_status

--- stock_inventory_turnover_report/__manifest__.py
+++ stock_inventory_turnover_report/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Inventory Turnover Report",
     "summary": "Analize inventory turnover in the last 6 and 12 months",
     "version": "12.0.1.0.0",
-    "development_status": "beta",
+    "development_status": "Beta",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
