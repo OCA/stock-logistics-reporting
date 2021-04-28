5845f7a10e3350f4c5a82ddba74f7e54dcce636c
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-04-28 23:22:36 +0200

    Eficent is now ForgeFlow

--- stock_report_quantity_by_location/__manifest__.py
+++ stock_report_quantity_by_location/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019-21 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Report Quantity By Location",
     "summary": "Stock Report Quantity By Location",
     "version": "13.0.1.0.0",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "category": "Warehouse Management",
     "license": "AGPL-3",
