419a48a9a10177f122f94e3058fd09425cf41de0
AaronHForgeFlow <ahenriquez@eficent.com>
2021-05-14 15:36:36 +0200

    [13.0][ADD] stock_inventory_valuation_remaining

---
+++ stock_inventory_valuation_show_remaining/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Inventory Valuation Show Remaining",
+    "version": "13.0.1.0.0",
+    "summary": "Remaining Inventory Valuation Report",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "category": "Warehouse Management",
+    "depends": ["stock_account"],
+    "license": "AGPL-3",
+    "data": ["views/stock_valuation_layer_views.xml"],
+    "installable": True,
+}
