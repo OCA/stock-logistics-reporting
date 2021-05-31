8b855e6d025e8bdf9c1c2b6bc1cc01df45ec6a60
Alfredo Zamora <alfredo.zamora@agilebg.com>
2021-05-31 11:32:56 +0200

    [IMP] stock_picking_comment_template: black, isort, prettier

--- stock_picking_comment_template/models/stock_picking.py
+++ stock_picking_comment_template/models/stock_picking.py
@@ -9,29 +9,31 @@ class StockPicking(models.Model):
 
     _inherit = "stock.picking"
 
-    comment_template1_id = fields.Many2one('base.comment.template',
-                                           string='Top Comment Template')
-    comment_template2_id = fields.Many2one('base.comment.template',
-                                           string='Bottom Comment Template')
-    note1 = fields.Html('Top Comment')
-    note2 = fields.Html('Bottom Comment')
+    comment_template1_id = fields.Many2one(
+        "base.comment.template", string="Top Comment Template"
+    )
+    comment_template2_id = fields.Many2one(
+        "base.comment.template", string="Bottom Comment Template"
+    )
+    note1 = fields.Html("Top Comment")
+    note2 = fields.Html("Bottom Comment")
 
-    @api.onchange('comment_template1_id')
+    @api.onchange("comment_template1_id")
     def _set_note1(self):
         comment = self.comment_template1_id
         if comment:
             self.note1 = comment.get_value(self.partner_id.id)
 
-    @api.onchange('comment_template2_id')
+    @api.onchange("comment_template2_id")
     def _set_note2(self):
         comment = self.comment_template2_id
         if comment:
             self.note2 = comment.get_value(self.partner_id.id)
 
-    @api.onchange('partner_id')
+    @api.onchange("partner_id")
     def _onchange_partner_id(self):
         comment_template = self.partner_id.comment_template_id
-        if comment_template.position == 'before_lines':
+        if comment_template.position == "before_lines":
             self.comment_template1_id = comment_template
-        elif comment_template.position == 'after_lines':
+        elif comment_template.position == "after_lines":
             self.comment_template2_id = comment_template
