d0900296e7d83f9dd6fea6f8339355cbb64ace7f
Saran440 <saranl@ecosoft.co.th>
2021-09-24 14:27:20 +0700

    [13.0][ADD] stock_inventory_valuation_unit_cost

---
+++ stock_inventory_valuation_unit_cost/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Inventory Valuation Unit Cost",
+    "version": "13.0.1.0.0",
+    "summary": "Add Unit Cost on Inventory Valuation Report",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "category": "Warehouse Management",
+    "depends": ["stock_account"],
+    "license": "AGPL-3",
+    "data": ["views/stock_valuation_layer_views.xml"],
+    "installable": True,
+}
