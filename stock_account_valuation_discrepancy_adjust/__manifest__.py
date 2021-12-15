c8dcd77ff219ebadaec59e6f49ea744cbae003e9
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-15 10:52:54 -0500

    [12.0][ADD] stock_account_valuation_discrepancy_adjust

---
+++ stock_account_valuation_discrepancy_adjust/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Valuation Discrepancy Adjust",
+    "version": "12.0.1.0.0",
+    "summary": "Implements Wizard for Adjust "
+    "Discrepancies on Account Inventory Valuation",
+    "category": "Warehouse Management",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "license": "AGPL-3",
+    "depends": ["stock_account_valuation_report"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/wizard_stock_discrepancy_report_view.xml",
+        "wizards/wizard_stock_discrepancy_adjustment_view.xml",
+        "views/product_discrepancy_view.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["ChrisOForgeFlow"],
+}
---
+++ stock_account_valuation_discrepancy_adjust/wizards/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import wizard_stock_discrepancy_report
+from . import wizard_stock_discrepancy_adjustment
