332cbc5ef8edb359c25a5016500da248a70dd117
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-02-18 09:00:48 +0100

    [MIG] stock_move_value_report: Migration to 13.0

--- stock_move_value_report/__manifest__.py
+++ stock_move_value_report/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Move Cost Value Report",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
