b5c1f8dcc4af7a71c5e66630bcb579a88a00bd0a
sergio-teruel <sergio.teruel@tecnativa.com>
2022-05-25 22:29:32 +0200

    [MIG] stock_picking_report_custom_description: Migration to v13.0

--- stock_picking_report_custom_description/__manifest__.py
+++ stock_picking_report_custom_description/__manifest__.py
@@ -6,14 +6,14 @@
 {
     "name": "Stock Picking Report Custom Description",
     "summary": "Show moves description in picking reports",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "development_status": "Production/Stable",
     "maintainers": ["carlosdauden"],
     "license": "AGPL-3",
-    "depends": ["stock",],
+    "depends": ["stock"],
     "data": [
         "views/report_deliveryslip.xml",
         "views/report_stockpicking_operations.xml",
