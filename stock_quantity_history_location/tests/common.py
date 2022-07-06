32bdcbc61751229d5e6bdaed6fc124b19835ef7f
AnnaPForgeFlow <anna.puig@forgeflow.com>
2022-07-06 13:31:32 +0200

    [MIG] stock_quantity_history_location: Migration 15.0
    Co-authored-by: DavidJForgeFlow <david.jimenez@forgeflow.com>

--- stock_quantity_history_location/tests/common.py
+++ stock_quantity_history_location/tests/common.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestCommon(SavepointCase):
+class TestCommon(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
