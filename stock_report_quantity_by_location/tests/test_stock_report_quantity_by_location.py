6edb9f48fac26ed036b4a6a0f14509fa60df52ba
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-07-13 10:41:44 +0530

    [MIG] stock_report_quantity_by_location: Migration to 15.0

--- stock_report_quantity_by_location/tests/test_stock_report_quantity_by_location.py
+++ stock_report_quantity_by_location/tests/test_stock_report_quantity_by_location.py
@@ -1,10 +1,10 @@
 # Copyright 2019-21 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestStockReportQuantityByLocation(SavepointCase):
+class TestStockReportQuantityByLocation(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestStockReportQuantityByLocation, cls).setUpClass()
