43aaf5323b60f25bfdd3c3ca72f6083863ab58e2
sergio-teruel <sergio.teruel@tecnativa.com>
2022-11-07 19:27:34 +0100

    [MIG] stock_picking_report_undelivered_product: Migration to v15.0

--- stock_picking_report_undelivered_product/models/product_template.py
+++ stock_picking_report_undelivered_product/models/product_template.py
@@ -7,6 +7,4 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    display_undelivered_in_picking = fields.Boolean(
-        string="Display undelivered in picking", default=True
-    )
+    display_undelivered_in_picking = fields.Boolean(default=True)
