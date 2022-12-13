f070983894143a58e73b58d5581709a86aeb975a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-12-13 19:49:49 +0100

    [FIX] stock_picking_report_custom_description: Transfer line description to move

---
+++ stock_picking_report_custom_description/models/stock_rule.py
@@ -0,0 +1,35 @@
+# Copyright 2022 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockRule(models.Model):
+    _inherit = "stock.rule"
+
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        company_id,
+        values,
+    ):
+        """Transfer the SO line description to the move name."""
+        res = super()._get_stock_move_values(
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            company_id,
+            values,
+        )
+        if values.get("sale_line_id"):
+            line = self.env["sale.order.line"].browse(values["sale_line_id"])
+            res["name"] = line.name
+        return res
