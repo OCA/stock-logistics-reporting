271e03c702d5a5f18c15ac452fc23d1e070866ea
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2023-03-16 18:29:13 +0100

    [MIG] stock_account_quantity_history_location: Migration to 15.0

--- stock_account_quantity_history_location/__manifest__.py
+++ stock_account_quantity_history_location/__manifest__.py
@@ -8,11 +8,10 @@
     "summary": """
         Glue module between Stock Account and Stock Quantity History Location
         modules""",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "depends": ["stock_account", "stock_quantity_history_location"],
-    "data": ["wizards/stock_quantity_history.xml"],
     "auto_install": True,
 }
