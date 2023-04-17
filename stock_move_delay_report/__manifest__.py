OCA/stock-logistics-workflow

[ADD] stock_move_delay_report

stock_move_delay_report/__manifest__.py
@@ -1,0 +1,16 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Move Delay Report",
+    "summary": "Stock Move Delay Report",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/delay_report_views.xml",
+    ],
+}

OCA/stock-logistics-reporting

stock_move_delay_report/__manifest__.py
@@ -1,0 +1,16 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Move Delay Report",
+    "summary": "Stock Move Delay Report",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/delay_report_views.xml",
+    ],
+}