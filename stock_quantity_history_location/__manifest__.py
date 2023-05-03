092cd9da36235897ec1eef891b6ec6f6d27e83fd
Hedguhar Domínguez <hedguhar@vauxoo.com>
2023-05-03 13:58:05 -0600

    [MIG] stock_quantity_history_location: Migration to 16.0

--- stock_quantity_history_location/__manifest__.py
+++ stock_quantity_history_location/__manifest__.py
@@ -5,9 +5,12 @@
 {
     "name": "Stock Quantity History Location",
     "summary": "Provides stock quantity by location on past date",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": [
+        "luisg123v",
+    ],
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "depends": ["stock"],
     "data": ["wizards/stock_quantity_history.xml"],
