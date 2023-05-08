0a653bb3e8205ad06977be0dbf13f10721c21a8a
dsolanki <dhara.solanki@initos.com>
2023-05-08 19:03:52 +0530

    [IMP] stock_card_report: pre-commit stuff

--- stock_card_report/__manifest__.py
+++ stock_card_report/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Card Report",
     "summary": "Add stock card report on Inventory Reporting.",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Ecosoft, Odoo Community Association (OCA)",
