d0c4a12b0e8235c5a8223ed8a708eeb44ac97b79
Bejaoui Souheil <souheil_bejaoui@hotmail.fr>
2023-10-04 14:21:49 +0200

    [IMP] stock_average_daily_sale: add contributor
    Co-authored-by: Jacques-Etienne Baudoux <je@bcim.be>

--- stock_average_daily_sale/__manifest__.py
+++ stock_average_daily_sale/__manifest__.py
@@ -7,7 +7,7 @@
         Allows to gather delivered products average on daily basis""",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,BCIM,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "depends": [
         "sale",
--- stock_average_daily_sale/readme/CONTRIBUTORS.rst
+++ stock_average_daily_sale/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Laurent Mignon <laurent.mignon@acsone.eu>
 * Denis Roussel <denis.roussel@acsone.eu>
+* Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
