b7806333744cf92577f2a597d3034021a0c9d1a0
Emilio Pascual <emilio@moduon.team>
2024-05-03 20:17:41 +0200

    [ADD] stock_picking_report_incoming_delivery_address
    MT-5963 @moduon

---
+++ stock_picking_report_incoming_delivery_address/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
+{
+    "name": "Stock Picking Report Incoming Delivery Address",
+    "summary": "Allow show delivery address in report when picking type is incoming",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Uncategorized",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["EmilioPascual", "rafaelbn"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_type.xml",
+        "views/report_deliveryslip.xml",
+        "views/report_stockpicking_operations.xml",
+    ],
+}
---
+++ stock_picking_report_incoming_delivery_address/readme/CONTEXT.md
@@ -0,0 +1,3 @@
+This module was developed because sometimes in incoming transaction types it is necessary to show the pick-up address, such as returns.
+
+It will be useful if the carrier has to pick up the goods at the address where they were delivered.
