3e32cf04dfa8b1bcd4ff57376f100a0ee789bc83
Carlos Roca <carlos.roca@tecnativa.com>
2024-05-29 10:30:47 +0200

    [ADD] stock_picking_report_header_repeater: New module to repeat the header in all pages of the delivery report

---
+++ stock_picking_report_header_repeater/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 Sergio Teruel - Tecnativa <sergio.teruel@tecnativa.com>
+# Copyright 2024 Carlos Roca - Tecnativa <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Report Header Repeater",
+    "version": "16.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/report_delivery_document.xml",
+    ],
+    "maintainers": [
+        "CarlosRoca13",
+        "sergio-teruel",
+    ],
+    "installable": True,
+}
