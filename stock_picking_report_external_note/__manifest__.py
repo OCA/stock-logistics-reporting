c228fd09584c425ddf9a5b207b6f8866a7f5a1cf
Eduardo De Miguel <edu@moduon.team>
2024-05-29 12:20:35 +0200

    [MIG] stock_picking_report_external_note: Migration to 16.0

--- stock_picking_report_external_note/__manifest__.py
+++ stock_picking_report_external_note/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Picking Report External Note",
     "summary": "Adds a note in Stock Picking shown on external reports like Delivery Slip",
-    "version": "15.0.0.1.1",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory/Delivery",
     "website": "https://github.com/OCA/stock-logistics-reporting",
