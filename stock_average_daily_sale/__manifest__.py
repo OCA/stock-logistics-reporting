deb1602cd747f02b615365ef40248943807ad78a
twalter-c2c <tomasz.walter@camptocamp.com>
2024-09-13 10:08:16 +0200

    stock_average_daily_sale: Pre-migration script

--- stock_average_daily_sale/__manifest__.py
+++ stock_average_daily_sale/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Average Daily Sale",
     "summary": """
         Allows to gather delivered products average on daily basis""",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,BCIM,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-reporting",
