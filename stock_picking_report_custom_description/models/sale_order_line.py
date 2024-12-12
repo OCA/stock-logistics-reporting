5b9947bd6bc589f07c06682e621e5ad646c2a445
David <david.vidal@tecnativa.com>
2024-12-12 13:31:26 +0100

    [FIX] stock_picking_report_custom_description: singleton error
    It isn't very common to write the name in batch. Anyway it would lead to
    errors in it's current shape.
    
    TT52171

--- stock_picking_report_custom_description/models/sale_order_line.py
+++ stock_picking_report_custom_description/models/sale_order_line.py
@@ -10,7 +10,5 @@ class SaleOrderLine(models.Model):
     def write(self, vals):
         res = super().write(vals)
         if vals.get("name"):
-            moves = self.env["stock.move"].search([("sale_line_id", "=", self.id)])
-            if moves:
-                moves.write({"description_picking": vals["name"]})
+            self.move_ids.description_picking = vals["name"]
         return res
