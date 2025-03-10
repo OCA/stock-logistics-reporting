599ec4c4ba3cff34ca4f0a6639ccc805d104a7a8
Bhavesh Heliconia <oca@heliconia.in>
2025-03-10 11:03:22 +0530

    [MIG] delivery_line_sale_line_position: Migration to 18.0

--- delivery_line_sale_line_position/__manifest__.py
+++ delivery_line_sale_line_position/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Delivery Line Sale Line Position",
     "summary": "Adds the sale line position to the delivery report lines",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Delivery",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
--- delivery_line_sale_line_position/readme/CONTRIBUTORS.md
+++ delivery_line_sale_line_position/readme/CONTRIBUTORS.md
@@ -1,3 +1,5 @@
 - Thierry Ducrest \<<thierry.ducrest@camptocamp.com>\>
 - [Trobz](https://trobz.com):
 - Do Anh Duy \<<duyda@trobz.com>\>
+- [Heliconia Solutions Pvt. Ltd.](https://www.heliconia.io)
+  - Bhavesh Heliconia
