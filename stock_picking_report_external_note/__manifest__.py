2ed068bc068b7366fe820b58dbedf60b0aef04a4
Bhavesh Heliconia <oca@heliconia.in>
2025-03-10 13:11:26 +0530

    [MIG] stock_picking_report_external_note: Migration to 18.0

--- stock_picking_report_external_note/__manifest__.py
+++ stock_picking_report_external_note/__manifest__.py
@@ -3,9 +3,10 @@
 
 {
     "name": "Stock Picking Report External Note",
-    "summary": "Adds a note in Stock Picking shown on external reports like Delivery Slip",
-    "version": "16.0.1.0.1",
-    "development_status": "Alpha",
+    "summary": "Adds a note in Stock Picking shown on external reports like "
+    "Delivery Slip",
+    "version": "18.0.1.0.1",
+    "development_status": "Beta",
     "category": "Inventory/Delivery",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Moduon, Odoo Community Association (OCA)",
--- stock_picking_report_external_note/readme/CONTRIBUTORS.md
+++ stock_picking_report_external_note/readme/CONTRIBUTORS.md
@@ -1,1 +1,3 @@
 - Eduardo de Miguel ([Moduon](https://www.moduon.team/))
+- [Heliconia Solutions Pvt. Ltd.](https://www.heliconia.io)
+  - Bhavesh Heliconia
