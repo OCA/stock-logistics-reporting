19d69a0b6c9a1caf385790b8cfb61834829b0c03
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-24 12:21:07 +0200

    [MIG] stock_picking_group_by_partner_by_carrier_sale_line_position: import module from 15.0

---
+++ stock_picking_group_by_partner_by_carrier_sale_line_position/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Stock Picking Group By Partner By Carrier Sale Line Position",
+    "summary": "Glue module for sale position and delivery report grouped",
+    "version": "14.0.1.0.2",
+    "category": "Delivery",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "depends": [
+        "delivery_line_sale_line_position",
+        "stock_picking_group_by_partner_by_carrier",
+    ],
+    "data": ["report/report_deliveryslip.xml"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ stock_picking_group_by_partner_by_carrier_sale_line_position/readme/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+This is a glue module for the sale line position on the delivery report
+and the module `stock_picking_group_by_partner_by_carrier`.
+
+It ensures that on the delivery report the position column is displayed
+only if some positions exist.
+And also adds the sale line position on the remaining to deliver list at
+the bottom of the report.
