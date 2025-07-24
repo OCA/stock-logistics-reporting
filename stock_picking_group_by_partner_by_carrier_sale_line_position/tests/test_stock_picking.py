3df29c1ceef7b31354abb63c55cc51cb26a9a4f9
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-24 12:56:36 +0200

    [FIX] pre-commit

--- stock_picking_group_by_partner_by_carrier_sale_line_position/tests/test_stock_picking.py
+++ stock_picking_group_by_partner_by_carrier_sale_line_position/tests/test_stock_picking.py
@@ -1,23 +1,37 @@
 from odoo.tests.common import TransactionCase
 
-class TestStockPickingOrder(TransactionCase):
 
+class TestStockPickingOrder(TransactionCase):
     def setUp(self):
         super().setUp()
-        Product = self.env['product.product'].create({
-            'name': 'Test Product',
-            'type': 'product',
-        })
-        SO = self.env['sale.order'].create({
-            'partner_id': self.env.ref('base.res_partner_12').id,
-        })
+        Product = self.env["product.product"].create(
+            {
+                "name": "Test Product",
+                "type": "product",
+            }
+        )
+        SO = self.env["sale.order"].create(
+            {
+                "partner_id": self.env.ref("base.res_partner_12").id,
+            }
+        )
 
-        SO.write({
-            'order_line': [
-                (0, 0, {'product_id': Product.id, 'product_uom_qty': 2, 'position': 2}),
-                (0, 0, {'product_id': Product.id, 'product_uom_qty': 1, 'position': 1}),
-            ],
-        })
+        SO.write(
+            {
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {"product_id": Product.id, "product_uom_qty": 2, "position": 2},
+                    ),
+                    (
+                        0,
+                        0,
+                        {"product_id": Product.id, "product_uom_qty": 1, "position": 1},
+                    ),
+                ],
+            }
+        )
         SO.action_confirm()
         self.picking = SO.picking_ids[0]
 
@@ -29,6 +43,5 @@ class TestStockPickingOrder(TransactionCase):
         report_lines = self.picking.get_delivery_report_lines()
 
         for line in report_lines:
-            if line.get('sale_order_line'):
-                self.assertIn('position_formatted', line)
-                
+            if line.get("sale_order_line"):
+                self.assertIn("position_formatted", line)
