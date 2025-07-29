e7a137731e7ab67f0e2fdc81d3ca0841570b744d
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-29 11:30:48 +0200

    fixed tests

--- stock_picking_group_by_partner_by_carrier_sale_line_position/tests/test_stock_picking_sort.py
+++ stock_picking_group_by_partner_by_carrier_sale_line_position/tests/test_stock_picking_sort.py
@@ -33,14 +33,14 @@ class TestStockPickingSort(common.TransactionCase):
         # Create a sale order with 3 lines and unordered positions
         SaleOrder = self.env["sale.order"]
         SaleLine = self.env["sale.order.line"]
-        self.order = SaleOrder.create(
+        self.order1 = SaleOrder.create(
             {
                 "partner_id": self.partner.id,
                 "carrier_id": carrier.id,
             }
         )
         # Lines with intentionally out-of-order positions
-        lines_data = [
+        lines_data1 = [
             {
                 "product_id": self.product.id,
                 "product_uom_qty": 5,
@@ -60,34 +60,69 @@ class TestStockPickingSort(common.TransactionCase):
                 "position": 20,
             },
         ]
-        for vals in lines_data:
+        for vals in lines_data1:
             SaleLine.create({"order_id": self.order.id, **vals})
 
-        # Create a picking from the sale order
-        self.order.action_confirm()
-        self.picking = self.order.picking_ids[0]
-        # Assign quantities to validate the lines
-        moves = self.picking.move_ids_without_package
-        for m in moves:
-            m.quantity_done = m.product_uom_qty
-        self.picking.button_validate()
+        self.order2 = SaleOrder.create(
+            {
+                "partner_id": self.partner.id,
+                "carrier_id": carrier.id,
+            }
+        )
+        lines_data2 = [
+            {
+                "product_id": self.product.id,
+                "product_uom_qty": 1,
+                "product_uom": uom.id,
+                "position": 5,
+            },
+            {
+                "product_id": self.product.id,
+                "product_uom_qty": 3,
+                "product_uom": uom.id,
+                "position": 15,
+            },
+        ]
+        for vals in lines_data2:
+            SaleLine.create({"order_id": self.order2.id, **vals})
 
-    def test_get_sorted_moves(self):
-        # Method parent defines some order, but here we check our wrapper
-        sorted_moves = self.picking._get_sorted_moves()
-        positions = [m.sale_line_id.position for m in sorted_moves]
+        # Confirmar les dues comandes i assignar quantitats
+        self.order1.action_confirm()
+        self.order2.action_confirm()
+        self.picking1 = self.order1.picking_ids[0]
+        self.picking2 = self.order2.picking_ids[0]
+
+        for picking in [self.picking1, self.picking2]:
+            for m in picking.move_ids_without_package:
+                m.quantity_done = m.product_uom_qty
+            picking.button_validate()
+
+    def test_get_sorted_moves_across_orders(self):
+        all_moves = (
+            self.picking1._get_sorted_moves() + self.picking2._get_sorted_moves()
+        )
+        positions = [
+            (m.sale_line_id.order_id.id, m.sale_line_id.position) for m in all_moves
+        ]
+        expected = sorted(positions, key=lambda x: x[0] * 1000 + x[1])
         self.assertEqual(
             positions,
-            [10, 20, 30],
-            "Els moviments no s'ordenen segons sale_line.position",
+            expected,
+            "Moves are not sorted by sale_line.position",
         )
 
-    def test_get_sorted_move_lines(self):
-        # Generate the move_lines (stock.move.line) and call the method
-        lines = self.picking._get_sorted_move_lines()
-        positions = [ml.move_id.sale_line_id.position for ml in lines]
+    def test_get_sorted_move_lines_across_orders(self):
+        all_lines = (
+            self.picking1._get_sorted_move_lines()
+            + self.picking2._get_sorted_move_lines()
+        )
+        positions = [
+            (ml.move_id.sale_line_id.order_id.id, ml.move_id.sale_line_id.position)
+            for ml in all_lines
+        ]
+        expected = sorted(positions, key=lambda x: x[0] * 1000 + x[1])
         self.assertEqual(
             positions,
-            [10, 20, 30],
-            "Les línies de moviment no s'ordenen segons sale_line.position",
+            expected,
+            "Move lines are not sorted by order_id and then by position",
         )
