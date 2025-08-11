362a9cbf049f836db05a79cd21d9b54f5e0c4ada
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2025-08-11 21:38:09 +0200

    [MIG] stock_quant_history: Migration to 18.0

--- stock_quant_history/__manifest__.py
+++ stock_quant_history/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Quant History",
     "summary": "Re-generate stock quants for given date",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Pierre Verkest <pierreverkest84@gmail.com>, "
     "Odoo Community Association (OCA)",
