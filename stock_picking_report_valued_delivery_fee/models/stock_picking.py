9fefecd32bb07473aed24673eb57360958209dcb
David Vidal <david@moduon.team>
2026-01-29 13:46:39 +0100

    [ADD] stock_picking_report_valued_delivery_fee: new module
    MT-13311

---
+++ stock_picking_report_valued_delivery_fee/models/stock_picking.py
@@ -0,0 +1,27 @@
+# Copyright 2026 Moduon
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def _compute_amount_all(self):
+        res = super()._compute_amount_all()
+        for picking in self:
+            fee_lines = picking.sale_id.order_line.filtered(
+                lambda x: x.delivery_fee_picking_id == picking
+            )
+            if not fee_lines:
+                continue
+            fee_amount_untaxed = sum(fee_lines.mapped("price_subtotal"))
+            fee_amount_taxed = sum(fee_lines.mapped("price_tax"))
+            fee_amount_total = fee_amount_untaxed + fee_amount_taxed
+            picking.update(
+                {
+                    "amount_untaxed": picking.amount_untaxed + fee_amount_untaxed,
+                    "amount_tax": picking.amount_tax + fee_amount_taxed,
+                    "amount_total": picking.amount_total + fee_amount_total,
+                }
+            )
+        return res
