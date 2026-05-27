fc6b445f50c5bdf6de9ebe34787926c9243879c2
Jacques-Etienne Baudoux <je@bcim.be>
2026-05-27 17:35:32 +0200

    [FIX] stock_average_daily_sale: company check

--- stock_average_daily_sale/models/stock_average_daily_sale_config.py
+++ stock_average_daily_sale/models/stock_average_daily_sale_config.py
@@ -11,7 +11,7 @@ from odoo.addons.stock_storage_type_putaway_abc.models.stock_location import (
 class StockAverageDailySaleConfig(models.Model):
     _name = "stock.average.daily.sale.config"
     _description = "Average daily sales computation parameters"
-    check_company_auto = True
+    _check_company_auto = True
 
     abc_classification_level = fields.Selection(
         selection=ABC_SELECTION, required=True, default="b"
