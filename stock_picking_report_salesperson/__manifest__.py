5a3ac28c717684a3f6b2d4538e7e429fa61e457f
Sadiq-OSI <sadiq.karjikar@graymatterlogic.com>
2026-06-26 15:31:42 -0400

    [19.0][MIG] stock_picking_report_salesperson: Migration to 19.0

--- stock_picking_report_salesperson/__manifest__.py
+++ stock_picking_report_salesperson/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Picking Report Salesperson",
     "summary": "Salesperson info in Stock Picking reports",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory/Inventory",
     "website": "https://github.com/OCA/stock-logistics-reporting",
